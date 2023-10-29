5c677753d313fd8df7ea172edc0995b90d4fdaae
ashitoshsable <ashitoshsable09@gmail.com>
2023-10-30 00:53:59 +0530

    css update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
     return (
         <div>
             <div class="container col-xxl-8 px-4 py-5">
-                <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-                    <div class="col-10 col-sm-8 col-lg-6">
+                <div class="row flex-lg-row-reverse align-items-center ">
+                    <div class="col-12 col-sm-12 col-lg-6">
                         <img src={HeroImg} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
                     </div>
                     <div class="col-lg-6">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,7 @@ const Navbar = () => {
     <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"></svg>
-        <span class="fs-2 fw-bold text-white">BlocSoc</span>
+        <img src={Logo} height={25}/>
       </a>
 
       <ul class="nav nav-pills ">
