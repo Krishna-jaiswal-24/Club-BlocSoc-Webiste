83584e25d4c7b1128195a8cc8a026aa6f11230f0
Krishna Jaiswal <krishnajaiswal2119@gmail.com>
2023-10-30 00:47:22 +0530

    added blogs page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import { Footer } from './components/Footer';
 import Team from './Pgaes/Teams Page/Team';
+import Blogs from './Pgaes/Blogs Page/Blogs';
 function App() {
   return (
     <div className="App">
-      {/* <Navbar />
+      <Navbar />
       <Hero />
-      <Footer /> */}
-      <Team />
+      <Blogs />
+      <Footer />
+      {/* <Team /> */}
+      
     </div>
   );
 }
