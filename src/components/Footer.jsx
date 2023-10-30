0c039e3026b1884f21f5264fdea5dca7f43ef716
ashitoshsable <ashitoshsable09@gmail.com>
2023-10-30 11:14:11 +0530

    teams

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,76 @@
-import React from 'react'
-import HeroImg from '../assests/BlocSoc-Transparent.png';
+import React from "react";
+import HeroImg from "../assests/BlocSoc-Transparent.png";
 
 export const Footer = () => {
-    return (
-        <div>
-            <div class="container">
-                <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top d-flex align-items-center">
-                    <div class="col mb-3">
-                        <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
-                            <img src={HeroImg} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
-                        </a>
-                        <p class="text-body-secondary"></p>
-                    </div>
+  return (
+    <div>
+      <div class="container">
+        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top text-white justify-content-center">
+          <div class="col mb-3">
+            <a
+              href="/"
+              class="d-flex align-items-center mt-0 link-body-emphasis text-decoration-none"
+            >
+              <img src={HeroImg} height={150} />
+            </a>
+            <p class="text-white">©blocsociiitdwd</p>
+          </div>
 
-                    <div class="col mb-3">
-                    </div>
-                    <div class="col mb-3">
-                    </div>
+          <div class="col mb-3"></div>
 
-                    <div class="col mb-3">
-                        <h5 className='text-white'>About Us</h5>
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0  text-white-50">Aim</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0  text-white-50">Vision</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0  text-white-50">Team</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0  text-white-50">Recruitment</a></li>
-                        </ul>
-                    </div>
+          <div class="col mb-3 text-white">
+            <h5>About US</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-white">
+                  Aim
+                </a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-white">
+                  Vision
+                </a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-white">
+                  Team
+                </a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-white">
+                  Recruitment
+                </a>
+              </li>
+            </ul>
+          </div>
 
-                    <div class="col mb-3">
-                        <h5 className='text-white'>Resources</h5>
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0  text-white-50">E-Books</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0  text-white-50">Youtube Courses</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0  text-white-50">Documentation</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0  text-white-50">Blogs</a></li>
-                        </ul>
-                    </div>
-
-                </footer>
-            </div>
-        </div>
-    )
-}
+          <div class="col mb-3 text-white">
+            <h5>Resources</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-white">
+                  E - books
+                </a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-white">
+                  Youtube Courses
+                </a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-white">
+                  Documentations
+                </a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-white">
+                  Blogs
+                </a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
-import HeroImg from '../assests/BlocSoc-Transparent.png';
+import React from "react";
+import HeroImg from "../assests/BlocSoc-Transparent.png";
 const Hero = () => {
-    return (
-        <div>
-            <div class="container col-xxl-8 px-4 py-5">
-                <div class="row flex-lg-row-reverse align-items-center ">
-                    <div class="col-12 col-sm-12 col-lg-6">
-                        <img src={HeroImg} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
-                    </div>
-                    <div class="col-lg-6">
-                        <h1 class="display-1 fw-bold text-white lh-1 mb-3">Your Step to the <span className='text-info'>Future</span>.</h1>
-                        <p class="text-white">Unlocking the powers of Blockchain. Blockchain club of IIIT Dharwad
-                            Create your Decentralised app today!</p>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button type="button" class="btn btn-info btn-lg px-4 me-md-2">UpComing Events</button>
-                            <button type="button" class="btn btn-outline-info btn-lg px-4">Explore BlocSoc</button>
-
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center ">
+          <div class="col-12 col-sm-12 col-lg-6">
+            <img
+              src={HeroImg}
+              class="d-block mx-lg-auto img-fluid"
+              alt="Bootstrap Themes"
+              width="700"
+              height="500"
+              loading="lazy"
+            />
+          </div>
+          <div class="col-lg-6">
+            <h1 class="display-1 fw-bold text-white lh-1 mb-3">
+              Your Step to the <span className="text-info">Future</span>.
+            </h1>
+            <p class="text-white">
+              Unlocking the powers of Blockchain. Blockchain club of IIIT
+              Dharwad Create your Decentralised app today!
+            </p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button type="button" class="btn btn-info btn-lg px-4 me-md-2">
+                UpComing Events
+              </button>
+              <button type="button" class="btn btn-outline-info btn-lg px-4">
+                Explore BlocSoc
+              </button>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
