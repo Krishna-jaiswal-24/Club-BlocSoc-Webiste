154946d2ed5e55a26c9d5c962898dbc145f9ef8b
Krishna Jaiswal <krishnajaiswal2119@gmail.com>
2023-11-05 22:49:54 +0530

    team data

---
+++ src/Pgaes/Teams Page/teamData.js
@@ -0,0 +1,27 @@
+let i=0
+
+const teamData=[{
+    imageSrc: "https://media.licdn.com/dms/image/C5616AQEinfrHvDdSKQ/profile-displaybackgroundimage-shrink_350_1400/0/1668366897738?e=1704931200&v=beta&t=uWqiyxiYBcqn9KAi-TXohdm4pQX0mym_dyjbzoEQxKw",
+    title: "Content Lead",
+    name: "Ashitosh Sable",
+    twitter:"https://twitter.com/ashitosh_png",
+    instagram:"https://www.instagram.com/ashitosh.png/",
+    linkedin:"https://www.linkedin.com/in/ashitosh-madhukar-sable/",
+    index:1,
+},{
+    imageSrc: "https://media.licdn.com/dms/image/D5603AQFuzRdfTccGXw/profile-displayphoto-shrink_400_400/0/1698641087977?e=1704931200&v=beta&t=UTzI6kg9uYBvzFihp8okRyB5JmqGgG_rW6MqPZ6gxh0",
+    title: "President",
+    name: "Ajay Bhakar",
+    twitter:"https://twitter.com/ajay_2512x",
+    instagram:"https://www.instagram.com/_ajay2512_/",
+    linkedin:"https://www.linkedin.com/in/ajay-bhakar-198510222/",
+    index:2,
+},{
+    imageSrc: "https://media.licdn.com/dms/image/D5603AQFuzRdfTccGXw/profile-displayphoto-shrink_400_400/0/1698641087977?e=1704931200&v=beta&t=UTzI6kg9uYBvzFihp8okRyB5JmqGgG_rW6MqPZ6gxh0",
+    title: "Member",
+    name: "Krishna Jaiswal",
+    twitter:"https://twitter.com/FrenzyDenji",
+    instagram:"https://www.instagram.com/denji.makima_/",
+    linkedin:"https://www.linkedin.com/in/krishna-jaiswal-16469215a/",
+    index:3,
+}]
